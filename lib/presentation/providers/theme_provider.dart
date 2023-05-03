624bcaa00863255b70a602a449a6245477fa9576
JToquica <jose.herley.2010@gmail.com>
2023-05-03 11:56:43 -0500

    Proyecto Terminado

--- lib/presentation/providers/theme_provider.dart
+++ lib/presentation/providers/theme_provider.dart
@@ -1,5 +1,4 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:widgets_app/config/theme/app_theme.dart';
 
 final isDarkModeProvider = StateProvider<bool>((ref) => true);
@@ -14,22 +13,6 @@ final themeNotifierProvider =
 class ThemeNotifier extends StateNotifier<AppTheme> {
   // State = Estado = new AppTheme();
   ThemeNotifier() : super(AppTheme());
-
-  Future<void> persistTheme() async {
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.setBool('isDarkMode', state.isDarkMode);
-    await prefs.setInt('selectedColor', state.selectedColor);
-  }
-
-  Future<void> loadTheme() async {
-    final prefs = await SharedPreferences.getInstance();
-    final isDarkMode = prefs.getBool('isDarkMode') ?? true;
-    final selectedColor = prefs.getInt('selectedColor') ?? 0;
-    state = AppTheme(
-      selectedColor: selectedColor,
-      isDarkMode: isDarkMode,
-    );
-  }
 
   void toggleDarkMode() {
     state = state.copyWith(isDarkMode: !state.isDarkMode);
