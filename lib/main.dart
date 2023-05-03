4c827926e0323b3121f5c8758c840e5e7fe8666c
JToquica <jose.herley.2010@gmail.com>
2023-05-03 11:46:18 -0500

    merge 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(
+    const ProviderScope(
+      child: MainApp(),
+    ),
+  );
 }
 
 class MainApp extends StatelessWidget {
